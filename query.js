a1a2cb38fc26c1319b870ea463aecd8703466d63
HelmiYogantara <helmiyogantara@gmail.com>
2018-06-25 22:37:52 +0700

    exercise done

--- query.js
+++ query.js
@@ -2,39 +2,57 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./data.db');
 
-var query = `select * from politicians`
-db.all(query, function(err, data){
-    if(err) throw err
-    console.log(data);
-    
-})
-
 //RElEASE 0 1
+function release01(){
+    var query = `SELECT name, location, grade_current, count(politicianId) AS totalVote
+    FROM politicians 
+    JOIN votes 
+    ON politicians.id = votes.politicianId
+    WHERE grade_current < 9
+    GROUP BY name
+    ORDER BY totalVote;`
+    
+    db.all(query,function(err,data){
 
-// SELECT name, location, grade_current, count(politicianId) AS totalVote
-// FROM politicians 
-// JOIN votes 
-// ON politicians.id = votes.politicianId
-// WHERE grade_current < 9
-// GROUP BY name
-// ORDER BY totalVote;
+        console.log(data);
+    })
+}
 
 
 //RELEASE 0 2
-// SELECT  totalVote,name,first_name ||" "|| last_name AS voterName, gender FROM votes,(SELECT count(*) as totalVote,politicians.name, politicians.id
-// FROM votes 
-// JOIN politicians 
-// ON  votes.politicianId = politicians.id
-// JOIN voters ON votes.voterId = voters.id
-// GROUP BY name
-// ORDER BY totalVote DESC
-// LIMIT 3) as newData LEFT JOIN voters ON votes.voterId = voters.id 
+function release02(){
+    var query = `SELECT newData.totalVotes, newData.politicianName, Voters.first_name || ' ' || Voters.last_name AS name, voters.gender 
+    FROM Votes,(SELECT COUNT(voterid) AS totalVotes, Politicians.name AS politicianName, Votes.politicianid FROM Politicians LEFT JOIN Votes 
+                     on Politicians.id = Votes.politicianid GROUP BY Votes.politicianid ORDER BY totalVotes DESC LIMIT 3) AS newData
+    LEFT JOIN Voters ON Votes.voterid = Voters.id
+    WHERE Votes.politicianid = newData.politicianid`
+
+    db.all(query,function(err,data){
+        console.log(data);
+    })
+}
 
 //RELEASE 0 3
-//select COUNT(votes.voterId) AS jumlahVote, first_name || ' ' || last_name AS name, gender, age 
-// FROM voters
-// JOIN votes
-// ON voters.id = votes.voterId
-// GROUP BY name
-// HAVING jumlahVote > 1
-// ORDER BY jumlahVote DESC 
+
+function release03(){
+    var query = `select COUNT(votes.voterId) AS jumlahVote, first_name || ' ' || last_name AS name, gender, age 
+    FROM voters
+    JOIN votes
+    ON voters.id = votes.voterId
+    GROUP BY name
+    HAVING jumlahVote > 1
+    ORDER BY jumlahVote DESC `
+    
+    db.all(query,function(err,data){
+        console.log(data);
+    })
+}
+
+
+release01()
+
+
+release02()
+
+
+release03()
