37b029a95db14aa1ea5e5a258a8c5ce55b752d74
kneric <brian.pradipta.a@gmail.com>
2018-06-27 23:39:36 +0700

    fix some query

--- index.js
+++ index.js
@@ -7,34 +7,42 @@ class Show {
     let query = `SELECT name, location, grade_current, COUNT(*) AS 'totalVotes' FROM Politicians INNER JOIN Votes ON Politicians.id = Votes.politician_id WHERE grade_current < 9 GROUP BY name ORDER BY grade_current ASC`;
 
     db.all(query, (err, result) => {
-      console.log(result);
+      if (err){
+        console.log(err.message)
+      } else {
+        console.log(result);
+      }
     });
   }
 
+
   static case2(){
-    let query = `SELECT newTable.totalVotes, newTable.name, Voters.first_name || " " || Voters.last_name AS voterName, Voters.gender FROM Votes, (SELECT COUNT (*) AS 'totalVotes', Politicians.name, Politicians.id
-    FROM Politicians
-    INNER JOIN Votes ON Politicians.id = Votes.politician_id 
-    GROUP BY Politicians.name
-    ORDER BY totalVotes DESC
-    LIMIT 3) AS newTable
-            INNER JOIN Voters ON Votes.voter_id = Voters.id
-            WHERE Votes.politician_id = newTable.id`;
+    let query = `SELECT newTable.totalVotes, newTable.name, Voters.first_name || " " || Voters.last_name AS voterName, Voters.gender FROM Votes INNER JOIN (SELECT COUNT (*) AS 'totalVotes', Politicians.name, Politicians.id FROM Politicians INNER JOIN Votes ON Politicians.id = Votes.politician_id GROUP BY Politicians.name ORDER BY totalVotes DESC LIMIT 3) AS newTable ON newTable.id = Votes.politician_id INNER JOIN Voters ON Votes.voter_id = Voters.id`;
 
     db.all(query, (err, result) => {
-      console.log(result);
+      if (err){
+        console.log(err.message)
+      } else {
+        console.log(result);
+      }
     });
   }
 
   static case3(){
-    let query = `SELECT (SELECT COUNT(*) FROM Votes WHERE voter_id = Voters.id) as 'totalVotes', first_name || ' ' || last_name AS name, gender, age FROM Voters WHERE totalVotes > 1 ORDER BY totalVotes desc`;
+    let query = `SELECT totalVotes, name, gender, age FROM (SELECT COUNT (*) AS 'totalVotes', first_name || ' ' || last_name AS name, 
+    gender, age FROM Voters INNER JOIN Votes ON Votes.voter_id = Voters.id GROUP BY Voters.id ORDER BY totalVotes desc) WHERE totalVotes >1
+    ORDER BY totalVotes DESC`;
 
     db.all(query, (err, result) => {
-      console.log(result);
+      if (err){
+        console.log(err.message)
+      } else {
+        console.log(result);
+      }
     });
   }
 }
 
-Show.case2()
+Show.case3()
 
 
